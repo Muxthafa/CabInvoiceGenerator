fbcf6767ad0c4c7ec33702db2471c8f7bbe0a5f2
muxthafa <musthafamohd0@gmail.com>
2021-09-27 15:52:13 +0530

    Added: aggregate fare of multiple rides

---
+++ main/java/com/bridgelabz/maven/invoicegenerator/RidesFare.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.maven.invoicegenerator;
+
+public class RidesFare {
+
+	double distance;
+	int time;
+	
+	public RidesFare(double distance, int time){
+		this.distance = distance;
+		this.time = time;
+	}
+}
--- test/java/com/bridgelabz/maven/invoicegenerator/CabInvoiceTest.java
+++ test/java/com/bridgelabz/maven/invoicegenerator/CabInvoiceTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz.maven.invoicegenerator;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,13 +11,25 @@ import org.junit.Test;
  */
 public class CabInvoiceTest 
 {
-
+	CabInvoice invoiceGenerator = null;
+	@Before
+	public void intializeClaa() {
+		invoiceGenerator = new CabInvoice();
+	}
     @Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
-		CabInvoice invoiceGenerator = new CabInvoice();
+		
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(25, fare, 0.0);
 	}
+    
+    @Test
+    public void givenDistanceAndTimeForMultipleRidesShouldReturnAggregate() {
+    	RidesFare[] rides = { new RidesFare(5.0,5), new RidesFare(2.0,3)};
+    	double fare = invoiceGenerator.calculateFare(rides);
+    	Assert.assertEquals(78.0, fare, 0.0);
+    }
+    
 }
